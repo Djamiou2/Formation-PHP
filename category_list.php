f62f2cdc06b6fb6306aa63ca245c8d3c26d88362
Nephilim/Coding Meta Hack <codingcity9014@gmail.com>
2021-06-12 10:54:46 +0100

    Rein de bon

--- category_list.php
+++ category_list.php
@@ -1,19 +1,22 @@
 <?php
-$title = 'Liste des catégories';
-require_once 'partials/_header.php';
+require_once 'includes/db.php';
+require_once 'includes/session_functions.php';
+require_once 'includes/functions.php';
 
-if (!logged_in()) redirect_to('login.php');
+$title = 'Liste des catégories';
+
+require_once 'partials/_header.php';
 
 $perPage = 10; //Nombre d'éléments à afficher par page
 
-$query = "SELECT c.id, title, c.created_at, user_id, name, firstname, email FROM category c JOIN user u ON c.user_id = u.id ";
-$queryCount = "SELECT COUNT(c.id) as count FROM category c JOIN user u ON c.user_id = u.id";
+$query = "SELECT * FROM category ";
+$queryCount = "SELECT COUNT(id) as count FROM category";
 $params = [];
 
 //Gestion des paramètre de la recherche
 if (!empty($_GET['q'])) {
-    $query .= " WHERE title LIKE :q OR name LIKE :q OR firstname LIKE :q";
-    $queryCount .= " WHERE title LIKE :q OR name LIKE :q OR firstname LIKE :q";
+    $query .= " WHERE title LIKE :q OR author LIKE :q";
+    $queryCount .= " WHERE title LIKE :q OR author LIKE :q";
     $params['q'] = "%{$_GET['q']}%";
 }
 
